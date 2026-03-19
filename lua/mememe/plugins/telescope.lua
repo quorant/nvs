246f91fb3114969903d05475cb09ae55b5944065
quorant <thereallyrealdd@gmail.com>
2026-03-19 01:54:20 -0400

    Added project.nvim

--- lua/mememe/plugins/telescope.lua
+++ lua/mememe/plugins/telescope.lua
@@ -9,6 +9,11 @@ return {
     local telescope = require("telescope")
     local builtin = require("telescope.builtin")
 
+    telescope.load_extension("projects")
+    vim.keymap.set("n", "<leader>fp", function()
+      telescope.extensions.projects.projects()
+    end, { desc = "Find projects" })
+
     vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
     vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
     vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
